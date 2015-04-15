899a7ba2accabedad4c31e341620d072553b234e
t.kurnosova <t.kurnosova@2gis.ru>
2015-04-15 17:44:52 +0600

    Add command ImplicidWait, IsElementDisplayed

---
+++ src/Winium.Desktop.Driver/CommandExecutors/IsElementDisplayedExecutor.cs
@@ -0,0 +1,30 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using System.Windows.Automation;
+
+    using Winium.Cruciatus.Extensions;
+    using Winium.StoreApps.Common;
+
+    #endregion
+
+    internal class IsElementDisplayedExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            var registeredKey = this.ExecutedCommand.Parameters["ID"].ToString();
+
+            var element = this.Automator.Elements.GetRegisteredElement(registeredKey);
+
+            var property = AutomationElementIdentifiers.IsOffscreenProperty;
+            var isDisplayd = ! element.GetAutomationPropertyValue<bool>(property);
+
+            return this.JsonResponse(ResponseStatus.Success, isDisplayd);
+        }
+
+        #endregion
+    }
+}
