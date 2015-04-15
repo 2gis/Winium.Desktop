cd2ba466514a615bbc3e7e8f58243e722d7188e3
t.kurnosova <t.kurnosova@2gis.ru>
2015-04-15 14:49:54 +0600

    Add method GetElementSize with tests

---
+++ src/TestApps.Tests/WindowsFormsTestApplication.Tests/GetElementSizeTests.cs
@@ -0,0 +1,29 @@
+﻿namespace WindowsFormsTestApplication.Tests
+{
+    #region using
+
+    using NUnit.Framework;
+
+    using OpenQA.Selenium;
+
+    #endregion
+
+    public class GetElementSizeTests : BaseTest
+    {
+        #region Public Methods and Operators
+
+        [TestCase("TextBox1", 200, 20)]
+        [TestCase("TextListBox", 200, 95)]
+        public void GetSizeOfElement(string elementId, int width, int height)
+        {
+            var element = this.Driver.FindElementById("Form1").FindElement(By.Id(elementId));
+
+            var size = element.Size;
+
+            Assert.AreEqual(width, size.Width);
+            Assert.AreEqual(height, size.Height);
+        }
+
+        #endregion
+    }
+}
