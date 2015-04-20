86750e1644d297ce0df4b7af9a4e41281e63cfd4
t.kurnosova <t.kurnosova@2gis.ru>
2015-04-20 17:50:43 +0600

    Add unimplemented tests to MouseDoubleClick command

---
+++ src/TestApps.Tests/WindowsFormsTestApplication.Tests/CommandTests/MouseDoubleClickTests.cs
@@ -0,0 +1,22 @@
+﻿namespace WindowsFormsTestApplication.Tests.CommandTests
+{
+    #region using
+
+    using NUnit.Framework;
+
+    #endregion
+
+    public class MouseDoubleClickTests : BaseTest
+    {
+        #region Public Methods and Operators
+
+        [Test]
+        [Ignore("Application not have needed elements")]
+        public void ClickToButton()
+        {
+            // TODO: Extend WindowsFormsTestApplication
+        }
+
+        #endregion
+    }
+}
