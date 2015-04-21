22471092e711a49eac1e7bdf6b28929e0b23285d
Golovin Gleb <g.golovin@2gis.ru>
2015-04-21 16:45:32 +0600

    Add executor for GetActiveElement command
    And update Winium.Cruciatus versions to 2.3.0

---
+++ src/Winium.Desktop.Driver/CommandExecutors/GetActiveElementExecutor.cs
@@ -0,0 +1,23 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using Winium.Cruciatus;
+    using Winium.StoreApps.Common;
+
+    #endregion
+
+    internal class GetActiveElementExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            var elementId = this.Automator.Elements.RegisterElement(CruciatusFactory.FocusedElement);
+            var webElement = new JsonWebElementContent(elementId);
+            return this.JsonResponse(ResponseStatus.Success, webElement);
+        }
+
+        #endregion
+    }
+}
