1600450e48d3f1be890b08bbf45b03659c63d9f8
Golovin Gleb <g.golovin@2gis.ru>
2015-04-21 16:47:39 +0600

    Add tests on GetActiveElement command

---
+++ src/TestApps.Tests/WindowsFormsTestApplication.Tests/CommandTests/GetActiveElementTests.cs
@@ -0,0 +1,28 @@
+﻿namespace WindowsFormsTestApplication.Tests.CommandTests
+{
+    #region using
+
+    using NUnit.Framework;
+
+    using OpenQA.Selenium;
+
+    #endregion
+
+    public class GetActiveElementTests : BaseForMainWindowTest
+    {
+        #region Public Methods and Operators
+
+        [Test]
+        public void GetActiveElement()
+        {
+            var textBox = this.MainWindow.FindElement(By.Id("TextBox1"));
+            textBox.Click();
+
+            var textBoxToo = this.Driver.SwitchTo().ActiveElement();
+
+            Assert.IsTrue(textBox.Equals(textBoxToo));
+        }
+
+        #endregion
+    }
+}
