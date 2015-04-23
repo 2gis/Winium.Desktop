66ace4a8fa9dfc75428c67f331bb94a7c9638b6d
Golovin Gleb <g.golovin@2gis.ru>
2015-04-23 11:33:48 +0600

    Add submit command implementation

---
+++ src/Winium.Desktop.Driver/CommandExecutors/SubmitElementExecutor.cs
@@ -0,0 +1,21 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using Winium.Cruciatus;
+
+    #endregion
+
+    internal class SubmitElementExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            CruciatusFactory.Keyboard.SendEnter();
+            return this.JsonResponse();
+        }
+
+        #endregion
+    }
+}
