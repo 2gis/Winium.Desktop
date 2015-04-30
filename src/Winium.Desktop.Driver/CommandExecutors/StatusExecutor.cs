c60b14fe599e71dd701b5b83dc8554607ade062a
Golovin Gleb <g.golovin@2gis.ru>
2015-04-30 20:17:55 +0600

    Add status command implementation

---
+++ src/Winium.Desktop.Driver/CommandExecutors/StatusExecutor.cs
@@ -0,0 +1,24 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using System.Collections.Generic;
+
+    using Winium.Desktop.Driver.CommandHelpers;
+    using Winium.StoreApps.Common;
+
+    #endregion
+
+    internal class StatusExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            var response = new Dictionary<string, object> { { "build", new BuildInfo() }, { "os", new OSInfo() } };
+            return this.JsonResponse(ResponseStatus.Success, response);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Winium.Desktop.Driver/CommandHelpers/BuildInfo.cs
@@ -0,0 +1,32 @@
+﻿namespace Winium.Desktop.Driver.CommandHelpers
+{
+    #region using
+
+    using System.Reflection;
+
+    using Newtonsoft.Json;
+
+    #endregion
+
+    public class BuildInfo
+    {
+        #region Static Fields
+
+        private static string version;
+
+        #endregion
+
+        #region Public Properties
+
+        [JsonProperty("version")]
+        public string Version
+        {
+            get
+            {
+                return version ?? (version = Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            }
+        }
+
+        #endregion
+    }
+}
