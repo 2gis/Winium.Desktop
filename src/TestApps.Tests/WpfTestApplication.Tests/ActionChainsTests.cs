7b8da301a1ed29bb65bc9ab1ad2ac3bf5553bb27
Yaroslav Lukyanov <c_sharp@mail.ru>
2015-06-28 22:58:47 +0600

    [*] Предложения

---
+++ src/TestApps.Tests/WpfTestApplication.Tests/ActionChainsTests.cs
@@ -0,0 +1,40 @@
+namespace WpfTestApplication.Tests
+{
+    #region using
+
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Interactions;
+    using OpenQA.Selenium.Remote;
+
+    #endregion
+
+    [TestFixture]
+    public class ActionChainsTests : BaseForMainWindowTest
+    {
+        #region Public Methods and Operators
+
+        [Test]
+        public void KeyUpAndKeyDown()
+        {
+            var list = this.MainWindow.FindElement(By.Id("TextListBox"));
+            var items = list.FindElements(By.ClassName("ListBoxItem"));
+            var first = items.First();
+            var random = items.ElementAt(3);
+            var actions = new Actions(this.Driver);
+
+            actions.Click(first).KeyDown(Keys.Shift).Click(random).KeyUp(Keys.Shift).Perform();
+
+            var selectedItemsCount = list.FindElements(By.ClassName("ListBoxItem")).Count(item => item.Selected);
+            
+            Assert.AreEqual(4, selectedItemsCount);
+        }
+        
+        #endregion
+    }
+}
