3278148055df948eb33fd64155dbfff2fe803665
t.kurnosova <t.kurnosova@2gis.ru>
2015-08-06 12:23:14 +0600

    Rename address for command GetComboBoxSelectedItem

--- src/Winium.Desktop.Driver/UriDispatchTables.cs
+++ src/Winium.Desktop.Driver/UriDispatchTables.cs
@@ -325,7 +325,7 @@
 
             this.commandDictionary.Add(
                 DriverCommand.FindComboBoxSelectedItem,
-                new CommandInfo("POST", "/session/{sessionId}/element/{id}/combobox/selected/element"));
+                new CommandInfo("POST", "/session/{sessionId}/element/{id}/combobox/item/selected"));
 
             this.commandDictionary.Add(
                 DriverCommand.ScrollToComboBoxItem,
