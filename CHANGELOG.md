6ddc2e8d2708816a50be2592c029a4e212542c1f
Golovin Gleb <g.golovin@2gis.ru>
2015-08-28 12:41:41 +0600

    Add changelog file. Prepare driver assembly info. Add ci script (for build and run tests).

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+﻿# Change Log
+
+<!--## Unreleased-->
+
+## v1.2.0
+
+New features:
+- Support Action Chains from bindings
+- Add new script command for setting value to element using ValuePattern
---
+++ scripts/setup.ps1
@@ -0,0 +1,36 @@
+﻿Param(
+    [string[]]$modules
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+#------------------------------
+
+$configuration = 'Release'
+$solution = Join-Path $PSScriptRoot '..\src\Winium.sln'
+$testFiles = ,"..\src\TestApps.Tests\WindowsFormsTestApplication.Tests\bin\$configuration\WindowsFormsTestApplication.Tests.dll"
+$testFiles += "..\src\TestApps.Tests\WpfTestApplication.Tests\bin\$configuration\WpfTestApplication.Tests.dll"
+$releaseDir = Join-Path $PSScriptRoot '..\Release'
+$artifactsDir = Join-Path $PSScriptRoot '..\Artifacts'
+$driverMerged = "..\src\Winium.Desktop.Driver\bin\$configuration\Merge\Winium.Desktop.Driver.exe"
+$errorLog = 'error.log'
+$outputLog = 'output.log'
+$assemblyInfoPath = Join-Path $PSScriptRoot '..\src\Winium.Desktop.Driver\Properties\AssemblyInfo.cs'
+$changelogPath = Join-Path $PSScriptRoot '..\CHANGELOG.md'
+$githubProjectName = 'Winium.Desktop.Driver'
+
+$msbuildProperties = @{
+    'Configuration' = $configuration
+}
+
+$modulesUrl = 'https://raw.githubusercontent.com/skyline-gleb/dev-help/v0.2.0/psm'
+
+if (!(Get-Module -ListAvailable -Name PsGet))
+{
+    (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+}
+
+foreach ($module in $modules)
+{
+    Install-Module -ModuleUrl "$modulesUrl/$module.psm1" -Update
+}
