74a09c7d456e8cf156519cd0c07eabd5b372f836
Golovin Gleb <g.golovin@2gis.ru>
2015-09-23 12:53:33 +0600

    Implementation GetCurrentWindowHandle and GetWindowHandles commands

---
+++ src/Winium.Desktop.Driver/CommandExecutors/GetCurrentWindowHandleExecutor.cs
@@ -0,0 +1,34 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using System.Globalization;
+    using System.Windows.Automation;
+
+    using Winium.StoreApps.Common;
+
+    #endregion
+
+    internal class GetCurrentWindowHandleExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            var node = AutomationElement.FocusedElement;
+            var rootElement = AutomationElement.RootElement;
+            var treeWalker = TreeWalker.ControlViewWalker;
+            while (node != rootElement && !node.Current.ControlType.Equals(ControlType.Window))
+            {
+                node = treeWalker.GetParent(node);
+            }
+
+            var result = (node == rootElement)
+                             ? string.Empty
+                             : node.Current.NativeWindowHandle.ToString(CultureInfo.InvariantCulture);
+            return this.JsonResponse(ResponseStatus.Success, result);
+        }
+
+        #endregion
+    }
+}
