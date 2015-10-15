494aa4cdf7ffd22934f996bbe000787511a58b61
zebraxxl <zebraxxl@mail.ru>
2015-10-16 01:26:52 +0300

    SwitchToWindow command implementation

---
+++ src/Winium.Desktop.Driver/CommandExecutors/SwitchToWindowExecutor.cs
@@ -0,0 +1,30 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using System.Windows.Automation;
+
+    using Winium.Cruciatus;
+    using Winium.Cruciatus.Core;
+
+    #endregion
+
+    internal class SwitchToWindowExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            var windowHandle = int.Parse(this.ExecutedCommand.Parameters["name"].ToString());
+
+            var handleProperty = AutomationElement.NativeWindowHandleProperty;
+            var window = CruciatusFactory.Root.FindElement(By.AutomationProperty(handleProperty, windowHandle));
+
+            window.SetFocus();
+
+            return this.JsonResponse();
+        }
+
+        #endregion
+    }
+}
