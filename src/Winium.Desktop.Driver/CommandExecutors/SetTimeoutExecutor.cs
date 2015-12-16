9e6672a9144c91b5c752a5e3b0afda29fb7499c5
James Johnson <james.johnson@Infuse.it>
2015-12-16 09:48:05 +0000

    Added command for SetTimeout.

---
+++ src/Winium.Desktop.Driver/CommandExecutors/SetTimeoutExecutor.cs
@@ -0,0 +1,36 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using System;
+
+    using Winium.Cruciatus;
+    using Winium.StoreApps.Common;
+
+    #endregion
+
+    internal class SetTimeoutExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            string type = this.ExecutedCommand.Parameters["type"].ToString();
+            var timeout = this.ExecutedCommand.Parameters["ms"];
+
+            if (type == "implicit")
+            {
+                CruciatusFactory.Settings.SearchTimeout = Convert.ToInt32(timeout);
+            }
+            else
+            {
+                string msg = string.Format("Winium does not implement timeout type '{0}'.", type);
+                throw new NotImplementedException(msg);
+            }
+
+            return this.JsonResponse();
+        }
+
+        #endregion
+    }
+}
