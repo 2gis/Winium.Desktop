6af9c4bf778232ebbebf06b6c03ba1c56513fb7a
quangnt <quang.nt@personifyinc.com>
2016-04-27 13:18:32 +0700

    Fix error when get some attributes, close launched application throws error Process Exited

--- src/Winium.Desktop.Driver/CommandExecutors/NewSessionExecutor.cs
+++ src/Winium.Desktop.Driver/CommandExecutors/NewSessionExecutor.cs
@@ -1,7 +1,7 @@
 ﻿namespace Winium.Desktop.Driver.CommandExecutors
 {
     #region using
-
+    using System;
     using System.Threading;
 
     using Newtonsoft.Json;
@@ -23,7 +23,7 @@
             // It is easier to reparse desired capabilities as JSON instead of re-mapping keys to attributes and calling type conversions, 
             // so we will take possible one time performance hit by serializing Dictionary and deserializing it as Capabilities object
             var serializedCapability =
-                JsonConvert.SerializeObject(this.ExecutedCommand.Parameters["desiredCapabilities"]);
+                JsonConvert.SerializeObject(this.ExecutedCommand.Parameters["desiredCapabilities"]);            
             this.Automator.ActualCapabilities = Capabilities.CapabilitiesFromJsonString(serializedCapability);
 
             this.InitializeApplication(this.Automator.ActualCapabilities.DebugConnectToRunningApp);
@@ -32,7 +32,65 @@
             // Gives sometime to load visuals (needed only in case of slow emulation)
             Thread.Sleep(this.Automator.ActualCapabilities.LaunchDelay);
 
+            // Update running application process in Application class if it's exited base on input process name
+            if (this.Automator.Application.HasExited())
+            {
+                // Add parse process name pass from request
+                var processName = (this.ExecutedCommand.Parameters["desiredCapabilities"]["processname"]);
+                // Update launched process by process name if it's exited
+                if (processName != null)
+                {
+                    this.Automator.Application.UpdateRunApplicationProcessBy(processName.ToString());
+                }
+            }
             return this.JsonResponse(ResponseStatus.Success, this.Automator.ActualCapabilities);
+            //if (this.Automator.Application.HasExited())
+            //{
+            //    try
+            //    {
+            //        // Add parse process name pass from request
+            //        var processName = (this.ExecutedCommand.Parameters["desiredCapabilities"]["processname"]).ToString();
+            //        //if (processName != null && processName != string.Empty)
+            //        //{
+            //            // Update launched process by process name if it's exited
+            //            var result = this.Automator.Application.UpdateRunApplicationProcessBy(processName);
+            //            if (!result)
+            //            {
+            //                return this.JsonResponse(ResponseStatus.UnableToGetLaunchedApplication, this.Automator.ActualCapabilities);
+            //            }
+            //        //}
+            //    }
+            //    catch (Exception)
+            //    {
+            //        return this.JsonResponse();
+            //    }
+            //}
+            //return this.JsonResponse(ResponseStatus.Success, this.Automator.ActualCapabilities);
+
+            //if (!this.Automator.Application.HasExited())
+            //{
+            //    return this.JsonResponse(ResponseStatus.Success, this.Automator.ActualCapabilities);
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        // Add parse process name pass from request
+            //        var processName = (this.ExecutedCommand.Parameters["desiredCapabilities"]["processname"]).ToString();
+
+            //        // Update launched process by process name if it's exited
+            //        var result = this.Automator.Application.UpdateRunApplicationProcessBy(processName);
+            //        if (result)
+            //        {
+            //            return this.JsonResponse(ResponseStatus.Success, this.Automator.ActualCapabilities);
+            //        }
+            //        return this.JsonResponse(ResponseStatus.UnableToGetLaunchedApplication, this.Automator.ActualCapabilities);
+            //    }
+            //    catch (Exception)
+            //    {
+            //        return this.JsonResponse();
+            //    }
+            //}
         }
 
         private void InitializeApplication(bool debugDoNotDeploy = false)
