2dbfd8bfb9af40394eaec55e379f7bb4c46cdb4c
David Dikman <david.dikman@infuse.it>
2016-05-06 14:10:58 +0100

    Added switch handler for frames

---
+++ src/DotNetRemoteWebDriver/CommandExecutors/SwitchToFrameExecutor.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetRemoteWebDriver.CommandExecutors
+{
+    internal class SwitchToFrameExecutor : CommandExecutorBase
+    {
+        protected override string DoImpl()
+        {
+            var frameHandle = ExecutedCommand.Parameters["name"].ToString();
+            Automator.Driver.SwitchTo().Frame(frameHandle);
+            return JsonResponse();
+        }
+    }
+}
