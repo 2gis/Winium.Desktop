f3be703e9a1789171c5d60caa0aa203dbbcadff1
David Dikman <david.dikman@infuse.it>
2016-05-09 06:41:08 +0100

    Tried toget files deployed to test folder using the DeploymentItem attribute

---
+++ src/DotNetRemoteWebDriverTests/GlobalSetup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetRemoteWebDriverTests
+{
+    [TestClass]
+    [DeploymentItem("wires.exe")]
+    [DeploymentItem("IEDriverServer.exe")]
+    [DeploymentItem("chromedriver.exe")]
+    [DeploymentItem("DotNetRemoteWebDriver.exe")]
+    public class GlobalSetup
+    {
+        [AssemblyInitialize]
+        public void Setup() { }
+    }
+}
