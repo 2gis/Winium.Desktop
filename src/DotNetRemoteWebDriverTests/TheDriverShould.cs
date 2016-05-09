a913ce723e7445826e920dbb069c259f00235f2d
David Dikman <david.dikman@infuse.it>
2016-05-09 17:08:29 +0100

    Made the test clean up even more properly

--- src/DotNetRemoteWebDriverTests/TheDriverShould.cs
+++ src/DotNetRemoteWebDriverTests/TheDriverShould.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using DotNetRemoteWebDriver;
@@ -11,26 +12,50 @@ namespace DotNetRemoteWebDriverTests
     [TestClass]
     public class TheDriverShould
     {
+        private readonly string[] _processNames = new[] {"chromedriver", "iedriverserver", "wires", "chrome", "firefox", "iexplore"};
         private Process _driverProcess;
 
         [TestMethod, TestCategory("Integration")]
         public void Can_Be_Started_After_Being_Killed()
         {
-            _driverProcess = Process.Start("DotNetRemoteWebDriver.exe", "--log-path driver.log");
-            Assert.IsNotNull(_driverProcess);
-            LaunchDriver(DesiredCapabilities.Firefox(), "http://google.com");
-            LaunchDriver(DesiredCapabilities.Chrome(), "http://google.com");
-            LaunchDriver(DesiredCapabilities.InternetExplorer(), "http://google.com");
+            var timeBeforeTest = new DateTime();
+            try
+            {
+                _driverProcess = Process.Start("DotNetRemoteWebDriver.exe", "--log-path driver.log");
+                Assert.IsNotNull(_driverProcess);
+                LaunchDriver(DesiredCapabilities.Firefox(), "http://google.com");
+                LaunchDriver(DesiredCapabilities.Chrome(), "http://google.com");
+                LaunchDriver(DesiredCapabilities.InternetExplorer(), "http://google.com");
 
-            _driverProcess.CloseMainWindow();
-            _driverProcess.KillAndWait();
+                _driverProcess.CloseMainWindow();
+                _driverProcess.KillAndWait();
 
-            _driverProcess = Process.Start("DotNetRemoteWebDriver.exe", "--log-path driver.log");
-            Assert.IsNotNull(_driverProcess);
-            Assert.IsFalse(_driverProcess.HasExited);
-            _driverProcess.KillAndWait();
+                _driverProcess = Process.Start("DotNetRemoteWebDriver.exe", "--log-path driver.log");
+                Assert.IsNotNull(_driverProcess);
+                Assert.IsFalse(_driverProcess.HasExited);
+                _driverProcess.KillAndWait();
+            }
+            finally
+            {
+                var killProcesses = Process.GetProcesses()
+                    .Where(p => _processNames.Contains(p.ProcessName.ToLower()))
+                    .Where(p => p.StartTime > timeBeforeTest)
+                    .ToArray();
+
+                foreach (var process in killProcesses)
+                {
+                    try
+                    {
+                        process.KillAndWait();
+                    }
+                    catch (Win32Exception)
+                    {
+                        Console.WriteLine($"So I couldn't kill this one process, {process.Id}, but that's fine..");
+                    }
+                }
+            }
         }
-        
+
         private void LaunchDriver(DesiredCapabilities browserCapabilities, string url)
         {
             var host = new Uri("http://localhost:4444/");
