153c0c153421721fbe4fcc1496985ec5cdf0fd61
David Dikman <david.dikman@infuse.it>
2016-06-09 09:25:45 +0100

    Implemented get_current_url

---
+++ src/DotNetRemoteWebDriver/CommandExecutors/GetCurrentUrlExecutor.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetRemoteWebDriver.CommandExecutors
+{
+    internal class GetCurrentUrlExecutor : CommandExecutorBase
+    {
+        protected override string DoImpl()
+        {
+            return JsonResponse(ResponseStatus.Success, Automator.Driver.Url);
+        }
+    }
+}
--- src/DotNetRemoteWebDriverTests/WithFirefoxTheDriverShould.cs
+++ src/DotNetRemoteWebDriverTests/WithFirefoxTheDriverShould.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
@@ -82,6 +81,18 @@ namespace DotNetRemoteWebDriverTests
             var inputs = _driver.FindElements(By.TagName("input"));
             var searchButton = inputs.Single(i => i.GetAttribute("name") == "btnK");
             Assert.AreEqual("Google Search", searchButton.GetAttribute("value"));
+        }
+
+        [TestMethod, TestCategory("Integration")]
+        public void Be_Able_To_Return_Url()
+        {
+            var remoteUrl = new Uri("http://localhost:4444/");
+            var capabilities = DesiredCapabilities.Firefox();
+            _driver = new RemoteWebDriver(remoteUrl, capabilities);
+
+            var url = "http://output.jsbin.com/bobiba/";
+            _driver.Navigate().GoToUrl(url);
+            Assert.AreEqual(url, _driver.Url);
         }
     }
 }
