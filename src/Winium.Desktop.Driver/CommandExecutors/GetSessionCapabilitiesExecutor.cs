f07421897b5521157915e21a7d5483e48bcd1932
Sven Jost <sven@wire.com>
2017-04-19 16:06:41 +0200

    Support for selenium 3.3

---
+++ src/Winium.Desktop.Driver/CommandExecutors/GetSessionCapabilitiesExecutor.cs
@@ -0,0 +1,20 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using Winium.StoreApps.Common;
+
+    #endregion
+
+    internal class GetSessionCapabilitiesExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            return this.JsonResponse(ResponseStatus.Success, this.Automator.ActualCapabilities);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Winium.Desktop.Driver/CommandExecutors/SetTimeoutExecutor.cs
@@ -0,0 +1,20 @@
+﻿namespace Winium.Desktop.Driver.CommandExecutors
+{
+    #region using
+
+    using Winium.StoreApps.Common;
+
+    #endregion
+
+    internal class SetTimeoutExecutor : CommandExecutorBase
+    {
+        #region Methods
+
+        protected override string DoImpl()
+        {
+            return this.JsonResponse(ResponseStatus.Success, null);
+        }
+
+        #endregion
+    }
+}
